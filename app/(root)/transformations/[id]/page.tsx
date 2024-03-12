7bd3306bffdd6b75516a4276698310a4b4e7c0c5
dale <stewartdale45@yahoo.com>
2024-03-12 18:04:07 -0400

    modified import statements in app/(root)/transformations/[id]/page.tsx

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -2,12 +2,15 @@ import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
-import Header from "@/components/shared/Header";
-import TransformedImage from "@/components/shared/TransformedImage";
+// import Header from "@/components/shared/Header";
+// import TransformedImage from "@/components/shared/TransformedImage";
 import { Button } from "@/components/ui/button";
 import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
-import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
+// import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
+import Header from "@/app/components/shared/Header";
+import TransformedImage from "@/app/components/shared/TransformedImage";
+import { DeleteConfirmation } from "@/app/components/shared/DeleteConfirmation";
 
 const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
   const { userId } = auth();
